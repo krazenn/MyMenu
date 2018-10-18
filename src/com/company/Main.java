b2ba9b6cae65803a5f1bcba734a3f6cc5e2da5a9
krazenn <antho13_1@msn.com>
2018-10-18 23:23:10 +0200

    ajout methode run menu

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Order order = new Order();
+        order.displayAvailableMenu();
+        Scanner sc = new Scanner(System.in);
+        int nb = sc.nextInt();
+        order.displaySelectedMenu(nb);
+    }
+}
---
+++ src/com/company/Order.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class Order {
+    /**
+     * Display all available menus in the restaurant.
+     */
+    public void displayAvailableMenu() {
+        System.out.println("Choix menu");
+
+        System.out.println("1 - poulet");
+
+        System.out.println("2 - boeuf");
+
+        System.out.println("3 - végétarien");
+
+        System.out.println("Que souhaitez-vous comme menu ?");
+
+    }
+    /**
+     * Display a selected menu.
+     * @param nbMenu The selected menu.
+     */
+    public void displaySelectedMenu(int nbMenu) {
+        switch (nbMenu) {
+            case 1:
+                System.out.println("Vous avez choisi comme menu : poulet");
+            break;
+            case 2:
+            System.out.println("Vous avez choisi comme menu : boeuf");
+            break;
+            case 3:
+            System.out.println("Vous avez choisi comme menu : végétarien");
+            break;
+            default:
+            System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+            break;
+        }
+    }
+    public void runMenu() {
+    }
+}
